47c6fad3c42821ece757ace572d02bcf13c7d2da
appchallenge <viet.pham@missionse.com>
2015-12-18 19:41:52 -0500

    Commented out problem code

--- app/src/main/java/com/example/mike/mseapp/LoginActivity.java
+++ app/src/main/java/com/example/mike/mseapp/LoginActivity.java
@@ -77,6 +77,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
+        /*
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
 
@@ -102,7 +103,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-
+*/
     }
 
     private void authenticateLogin(View view) {
